1b0aa78730f58804e62c0cb974d798ecb4dc7bd3
DURAN Ivan <ivan.duran@outlook.fr>
2025-02-17 20:18:59 +0100

    allow for  robot

--- app/robots.tsx
+++ app/robots.tsx
@@ -1,11 +1,17 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
-    sitemap: 'https://idevstudio.fr/sitemap.xml'
-  }
-}
+    return {
+        rules: [
+          {
+            userAgent: '*',
+            allow: '/',
+          },
+          {
+            userAgent: '*',
+            allow: 'https://www.gstatic.com/draco/versioned/decoders/1.5.2/draco_decoder.wasm',
+          }
+        ],
+        sitemap: 'https://idevstudio.fr/sitemap.xml'
+      }
+    }
