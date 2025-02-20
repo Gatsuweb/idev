706852acbdc488e7de155df44b54f201f209d8b8
DURAN Ivan <ivan.duran@outlook.fr>
2025-02-20 10:41:11 +0100

    preload

--- app/components/Preload.tsx
+++ app/components/Preload.tsx
@@ -1,26 +1,26 @@
 "use client"
 import Image from "next/image"
 import s from "../styles/Preload.module.css"
-import { useEffect } from "react";
-import { useRouter } from "next/navigation" 
+// import { useEffect } from "react";
+// import { useRouter } from "next/navigation" 
 export const Preload = () => {
-  const router = useRouter(); 
+  // const router = useRouter(); 
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      router.push('/'); 
-    }, 6000);
-    return () => clearTimeout(timer);
-  }, [router]);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     router.push('/'); 
+  //   }, 6000);
+  //   return () => clearTimeout(timer);
+  // }, [router]);
   return (
-    <>
-    <div className={s.preloadContainer}>
+    <div className={s.preload}>
+      <div className={s.preloadContainer}>
 
-            <p className={s.titlePreload}>Bienvenue au studio I&apos;DEV.</p>
-     </div>
+              <p className={s.titlePreload}>Bienvenue au studio I&apos;DEV.</p>
+      </div>
 
-    <div className={s.preloadContainers}>
-        <Image src="/afou2bis.webp" alt="" height={3000} width={2000} className={s.imgPreload} priority />
+      <div className={s.preloadContainers}>
+          <Image src="/afou2bis.webp" alt="" height={3000} width={2000} className={s.imgPreload} priority />
+  </div>
  </div>
- </>
   )}
