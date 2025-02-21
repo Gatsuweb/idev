46652c2ff8bd69ccd3b4e5002c406e06abec05ed
DURAN Ivan <ivan.duran@outlook.fr>
2025-02-21 17:57:08 +0100

    google analytic scipt

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Script from "next/script";
 
 export const generateMetadata = (): Metadata => {
   const baseUrl = "https://idevstudio.fr";
@@ -138,6 +139,22 @@ export default function RootLayout({
     <html lang="fr">
       <body>
         {children}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-0KCZRHLBMV"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-0KCZRHLBMV');
+            `,
+          }}
+        />
       </body>
     </html>
   );
